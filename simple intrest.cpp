6847e9305a93f0a7da854daffd4713c7a57b9749
clemstone mwendwa <161031298+clemoh1@users.noreply.github.com>
2024-09-22 10:56:17 +0300

    Add files via upload

---
+++ simple intrest.cpp
@@ -0,0 +1,26 @@
+//calculating simple intrest
+#include <stdio.h> scanf() , printf()
+int main(){
+	amount =b;
+	time =c;
+	rate =d;
+	simple_intrest;
+		
+	printf("enter  amount: ");
+	scanf("%lf",&b);
+printf("the amount is %lf \n",b);
+
+	printf("enter time: ");
+	scanf("%d",&c);
+printf("the time is %d \n",c);
+
+	printf("enter time: ");
+	scanf("%f",&);
+printf("the character is %f \n",d);
+
+simple_intrest=("amount*time*rate)/100 \n");
+
+printf("the simple intrest is %d \n",simple_intrest);
+
+return 0;
+}
